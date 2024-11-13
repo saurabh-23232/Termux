823e6fa28b5237843875c87f7995eae1b281ec5a
Saurabh Kumar <76238822+saurabh-23232@users.noreply.github.com>
2024-11-13 16:00:40 +0530

    Update setup.sh

--- Style/setup.sh
+++ Style/setup.sh
@@ -1,10 +1,28 @@
-# Set background to black and text color to yellow and dark blue
-echo -e "\e[48;5;0m"  # Set background color to black
-echo -e "\e[38;5;226m"  # Set text color to yellow (color code 226)
+#!/bin/bash
 
-# Print "Devil Hacking" in ASCII art using purple text color
-echo -e "\e[38;5;93m"  # Set text color to purple (color code 93)
-cat << "EOF"
+# Check if the user is using bash or zsh
+SHELL_CONFIG_FILE="$HOME/.bashrc"  # Default to .bashrc for bash users
+
+if [[ $SHELL == *"zsh"* ]]; then
+  SHELL_CONFIG_FILE="$HOME/.zshrc"  # Use .zshrc for zsh users
+fi
+
+# Check if the .bashrc or .zshrc file exists
+if [ ! -f "$SHELL_CONFIG_FILE" ]; then
+  touch "$SHELL_CONFIG_FILE"  # Create the file if it doesn't exist
+fi
+
+# Add banner code to .bashrc or .zshrc (depending on shell)
+echo "Adding custom banner to $SHELL_CONFIG_FILE..."
+
+cat << EOF >> "$SHELL_CONFIG_FILE"
+
+# Custom Banner
+echo -e "\e[48;5;0m"  # Set background color to black
+echo -e "\e[38;5;226m"  # Set text color to yellow
+echo -e "\e[38;5;93m"  # Set text color to purple for the banner
+
+cat << 'BANNER'
   _____             _ _   _    _            _    _             
  |  __ \           (_) | | |  | |          | |  (_)            
  | |  | | _____   ___| | | |__| | __ _  ___| | ___ _ __   __ _ 
@@ -13,7 +31,11 @@ cat << "EOF"
  |_____/ \___| \_/ |_|_| |_|  |_|\__,_|\___|_|\_\_|_| |_|\__, |
                                                           __/ |
                                                          |___/  
+BANNER
+
+# Reset color to default
+echo -e "\e[0m"
+
 EOF
 
-# Reset color to default after banner
-echo -e "\e[0m"
+echo "Banner added successfully!"
